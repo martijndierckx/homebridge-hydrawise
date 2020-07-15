10143fc96782fee79d5cd28326cfbcf0deeec7f4
martijndierckx <martijn@smard-media.be>
2020-07-15 10:05:25 +0200

    Added PLUGIN_NAME

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 const settings_1 = require("./settings");
 const HydrawisePlatform_1 = require("./HydrawisePlatform");
 module.exports = (api) => {
-    api.registerPlatform(settings_1.PLATFORM_NAME, HydrawisePlatform_1.HydrawisePlatform);
+    api.registerPlatform(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, HydrawisePlatform_1.HydrawisePlatform);
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import type { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
+import { PLUGIN_NAME, PLATFORM_NAME } from './settings';
 import { HydrawisePlatform } from './HydrawisePlatform'; 
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, HydrawisePlatform);
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, HydrawisePlatform);
 }
